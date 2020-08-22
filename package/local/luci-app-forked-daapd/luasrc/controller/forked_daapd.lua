e33f790110d722125f0fa0ef604b238efd883548
Liangbin Lian <jjm2473@gmail.com>
2020-08-22 15:52:03 +0800

    luci-app-forked-daapd: support config multi path, show server status

--- package/local/luci-app-forked-daapd/luasrc/controller/forked_daapd.lua
+++ package/local/luci-app-forked-daapd/luasrc/controller/forked_daapd.lua
@@ -12,4 +12,13 @@ function index()
 
 	page = entry({"admin", "nas", "forked_daapd"}, cbi("forked_daapd"), _("iTunes"))
 	page.dependent = true
+
+	entry({"admin", "nas", "forked_daapd", "status"}, call("api_status")).leaf=true
+end
+
+function api_status()
+	local e={}
+	e.running=luci.sys.call("pgrep -x /usr/sbin/forked-daapd >/dev/null")==0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
--- package/local/luci-app-forked-daapd/luasrc/model/cbi/forked_daapd.lua
+++ package/local/luci-app-forked-daapd/luasrc/model/cbi/forked_daapd.lua
@@ -5,17 +5,26 @@ local m, s, o
 m = Map("forked-daapd", "iTunes Server",
 	translate("iTunes (DAAP) server for Apple Remote and AirPlay."))
 
+m:section(SimpleSection).template  = "forked_daapd_status"
+
 s = m:section(TypedSection, "forked-daapd", translate("Settings"))
 s.anonymous = true
 
 o = s:option(Flag, "enabled", translate("Enable"))
 o.rmempty = false
 
+o = s:option(Value, "port", translate("Port:"))
+o.datatype = "port"
+o.default = 3689
+o.placeholder = "3689"
+o.rmempty = false
+
 servername = s:option(Value, "name", translate("Server Name"))
 servername.rmempty = true
 
-path = s:option(Value, "path", translate("Media Folder"))
+path = s:option(DynamicList, "path", translate("Media directories:"))
 path.rmempty = true
+path.placeholder = "/mnt/music"
 
 db_path = s:option(Value, "db_path", translate("Database Path"))
 db_path.rmempty = true
