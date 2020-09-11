ea2b5b878605c61ad0f4cb5245cc8e720eeec21e
Liangbin Lian <jjm2473@gmail.com>
2020-09-11 23:43:36 +0800

    ci: readable version

--- .github/workflows/diy-part1.sh
+++ .github/workflows/diy-part1.sh
@@ -13,3 +13,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# version
+TZ='Asia/Shanghai' echo "r`date '+%y%m%d'`-`git log -n 1 --format='%H'|cut -c-8`" > version
